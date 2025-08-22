9a3ef9a5a507c4cc611c6f59169a185cea978db4
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-08-22 10:52:39 +0530

    min area to cover all ones

---
+++ Daily/minAreatocoverallOnes.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int minimumArea(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        int maxRow=0,maxCol=0;
+        int minRow=m-1,minCol=n-1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    minRow=min(minRow,i);
+                    maxRow=max(maxRow,i);
+                    minCol=min(minCol,j);
+                    maxCol=max(maxCol,j);
+                }
+            }
+        }
+        return (maxRow-minRow+1)*(maxCol-minCol+1);
+    }
+};
