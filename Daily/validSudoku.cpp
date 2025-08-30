4bd5072c2b9b6f3ce57cc76e226431e2528efbe4
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-08-30 11:09:29 +0530

    solution optimised

--- Daily/validSudoku.cpp
+++ Daily/validSudoku.cpp
@@ -2,6 +2,7 @@
 #include<unordered_set>
 #include<string>
 using namespace std;    
+
 class Solution {
 public:
     bool validBox(vector<vector<char>>& board,int sr,int sc,int er,int ec){
@@ -47,9 +48,10 @@ public:
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.') continue;
-                string row=string(1,board[i][j])+"  row "+to_string(i)+" ";
-                string col=string(1,board[i][j])+"  col "+to_string(j)+" ";
-                string box=string(1,board[i][j])+" box  "+to_string(i/3)+" "+to_string(j/3);
+                int boxNum=(i/3)*3+(j/3);
+                string row="r"+to_string(i)+board[i][j];
+                string col="c"+to_string(j)+board[i][j];
+                string box="b"+to_string(boxNum)+board[i][j];
                 if(st.find(row)!=st.end() || st.find(col)!=st.end() || st.find(box)!=st.end()) return false;
                 st.insert(row);
                 st.insert(col);
