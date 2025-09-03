6a0ae26bac0f460c7b316143a5274cbf55ab5079
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-09-03 20:11:23 +0530

    minimum cost for tickets

---
+++ DP/minCostForTickets.cpp
@@ -0,0 +1,48 @@
+#include<vector>
+#include<algorithm>
+#include<unordered_map>
+#include<string>                
+using namespace std;
+
+class Solution {
+public:
+int n;
+int t[366];
+    int solve(vector<int>& days,vector<int>& costs,int i){
+        if(i>=n) return 0;
+        if(t[i]!=-1) return t[i];
+        int path1=costs[0]+solve(days,costs,i+1);
+        int j=i;
+        int maxday=days[i]+7;
+        while(j<n && days[j]<maxday){
+            j++;
+        }
+        int path2=costs[1]+solve(days,costs,j);
+        int k=i;
+        maxday=days[i]+30;
+        while(k<n && days[k]<maxday) k++;
+        int path3=costs[2]+solve(days,costs,k);
+        return t[i]=min({path1,path2,path3});
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+         n=days.size();
+        //  memset(t,-1,sizeof(t));
+        // return solve(days,costs,0);
+        vector<int> dp(n,0);
+        //state defn:- dp[i]->minimum number of dollars to travel ith  to nth day
+        dp[n-1]=min({costs[0],costs[1],costs[2]});
+        for(int i=n-2;i>=0;i--){
+            int path1=costs[0]+dp[i+1];
+            int j=i;
+            int maxi=days[i]+7;
+            while(j<n && days[j]<maxi && maxi<=days[n-1]) j++;
+            int path2=costs[1]+dp[j];
+            int k=i;
+            int maxi2=days[i]+30;
+            while(k<n && days[k]<maxi2 && maxi2<=days[n-1]) k++;
+            int path3=costs[2]+dp[k];
+            dp[i]=min({path1,path2,path3});
+        }
+        return dp[0];
+    }
+};
