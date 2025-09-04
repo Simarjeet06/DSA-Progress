a9832861b8308a5d900b174eeb228f6257c6ab98
Simarjeet Kaur <simarjeetkaur6603@gmail.com>
2025-09-04 11:43:17 +0530

    find closest person

---
+++ Daily/findClosestPerson.cpp
@@ -0,0 +1,12 @@
+#include<cmath>
+using namespace std;
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int p1=abs(z-x);
+        int p2=abs(z-y);
+        if(p1==p2) return 0;
+        else if(p1<p2) return 1;
+        return 2;
+    }
+};
